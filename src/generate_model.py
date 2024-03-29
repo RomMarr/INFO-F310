f1215264f13c2cb22ba6526fbf3d18797416f664
Romain <romain.markowitch@ulb.be>
2024-03-29 12:07:31 +0100

    conditions check

--- src/generate_model.py
+++ src/generate_model.py
@@ -1,10 +1,31 @@
-#import argparse
-def main():
-    pass
+import sys
+import os
+
+def isFileInFolder(fileName):
+    # Get a list of all files in the folder
+    filesInFolder = os.listdir("instances")
+
+    # Check if the file's name matches any of the files in the folder
+    return fileName in filesInFolder
+
+
+
+def main(instanceName, p):
+    if not isFileInFolder(instanceName):  # Check if the file exists in the instances folder
+        raise ValueError("File name does not exist in instances folder")
+    else: 
+        p = int(p)  # Convert p to an integer
+        if p == 0:
+            print("p is 0")
+        elif p ==1:
+            print("p is 1")
+        else:  # P parameter does not match the expected values
+            raise ValueError("Parameter p must be 0 or 1")
 
 
 
 
 if __name__ == '__main__':
-    main()
-#   args = parser.parse_args()
+    instanceName = sys.argv[1]  # instance's file's name
+    p = sys.argv[2]  # p parameter
+    main(instanceName, p)
